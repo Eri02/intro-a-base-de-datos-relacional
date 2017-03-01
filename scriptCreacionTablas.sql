c7b4c42181442bef1ec2a105c766ad5977deedff
Erika Roque <erikaroque2@gmail.com>
2017-03-01 00:55:10 -0300

    Creación de tablas e inserción de datos

---
+++ scriptCreacionTablas.sql
@@ -0,0 +1,47 @@
+USE BDPTF; /*Indico qué base de datos usar*/
+
+/*Creación de tablas*/
+CREATE TABLE Provincias
+(codProvincia SMALLINT PRIMARY KEY AUTO_INCREMENT,
+nombreProvincia VARCHAR(60)
+);
+
+CREATE TABLE Productos
+(codProducto SMALLINT PRIMARY KEY,
+nombreProducto VARCHAR(60) NOT NULL,
+precioUnitario NUMERIC(12,2)
+);
+
+CREATE TABLE Clientes
+(codCliente INTEGER PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(60) NOT NULL,
+apellido VARCHAR(60) NOT NULL,
+nroCuit BIGINT UNIQUE,
+empresa VARCHAR(60), /* eliminar columna*/ 
+direccion1 VARCHAR(60),
+direccion2 VARCHAR(60), /* Eliminar columna*/
+codProvincia SMALLINT REFERENCES Provincias,
+ciudad VARCHAR(60),
+codPostal VARCHAR(10),
+telefono VARCHAR(60)
+);
+
+/* codCliente, nombre, apellido, nroCuit, empresa, direccion1, codPronvincia, ciudad, codPostal, telefono */
+
+CREATE TABLE Facturas
+(nroFactura INTEGER PRIMARY KEY AUTO_INCREMENT,
+fechaFactura DATE NOT NULL,
+codCliente INTEGER NOT NULL REFERENCES Clientes,
+fechaVencimiento DATE NOT NULL CHECK (fechaVencimiento >= fechaFactura)
+);
+
+CREATE TABLE FacturasDetalle
+(nroFactura INTEGER REFERENCES Facturas,
+nroItem SMALLINT,
+codProducto INTEGER REFERENCES Productos,
+precioUnitario NUMERIC(12,2),
+cantidad INTEGER NOT NULL CHECK (cantidad >0),
+PRIMARY KEY (nroFactura,nroItem)
+);
+
+/* ********************************************************************************************************* */
