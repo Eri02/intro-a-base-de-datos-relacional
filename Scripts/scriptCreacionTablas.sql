11e943271a1f8b432f5c43ead8e26b298ae7f57d
Erika Roque <erikaroque2@gmail.com>
2017-03-16 16:09:59 -0300

    correción de DER, creación de tablas modificado

--- Scripts/scriptCreacionTablas.sql
+++ Scripts/scriptCreacionTablas.sql
@@ -3,45 +3,44 @@ USE BDPTF; /*Indico qué base de datos usar*/
 /*Creación de tablas*/
 
 CREATE TABLE Barrios
-(codBarrio SMALLINT PRIMARY KEY AUTO_INCREMENT,
-nombreBarrio VARCHAR(60)
+(ba_codBarrio SMALLINT PRIMARY KEY AUTO_INCREMENT,
+ba_nombreBarrio VARCHAR(60)
 );
 
 CREATE TABLE Productos
-(codProducto SMALLINT PRIMARY KEY,
-nombreProducto VARCHAR(60) NOT NULL,
-precioUnitario NUMERIC(12,2)
+(pro_codProduto SMALLINT PRIMARY KEY,
+pro_nombreProducto VARCHAR(60) NOT NULL,
+pro_precioUnitario NUMERIC(12,2)
 );
-
+USE BDPTF;
 CREATE TABLE Clientes
-(codCliente INTEGER PRIMARY KEY AUTO_INCREMENT,
-nombre VARCHAR(60) NOT NULL,
-apellido VARCHAR(60) NOT NULL,
-nroCuit BIGINT UNIQUE,
-empresa VARCHAR(60), /* eliminar columna*/ 
-direccion1 VARCHAR(60),
-codBarrio SMALLINT REFERENCES Barrios,
-ciudad VARCHAR(60),
-codPostal VARCHAR(10),
-telefono VARCHAR(60)
+(cli_codCliente INTEGER PRIMARY KEY AUTO_INCREMENT,
+cli_nombre VARCHAR(60) NOT NULL,
+cli_apellido VARCHAR(60) NOT NULL,
+cli_nroCuit BIGINT UNIQUE,
+cli_direccion VARCHAR(60),
+cli_codBarrio SMALLINT REFERENCES Barrios,
+cli_ciudad VARCHAR(60),
+cli_codPostal VARCHAR(10),
+cli_telefono VARCHAR(60)
 );
 
 /* codCliente, nombre, apellido, nroCuit, empresa, direccion1, codPronvincia, ciudad, codPostal, telefono */
 
 CREATE TABLE Facturas
-(nroFactura INTEGER PRIMARY KEY AUTO_INCREMENT,
-fechaFactura DATE NOT NULL,
-codCliente INTEGER NOT NULL REFERENCES Clientes,
-fechaVencimiento DATE NOT NULL CHECK (fechaVencimiento >= fechaFactura)
+(f_nroFactura INTEGER PRIMARY KEY AUTO_INCREMENT,
+f_fechaFactura DATE NOT NULL,
+f_codCliente INTEGER NOT NULL REFERENCES Clientes,
+f_fechaVencimiento DATE NOT NULL CHECK (fechaVencimiento >= fechaFactura)
 );
-
+USE BDPTF;
 CREATE TABLE FacturasDetalle
-(nroFactura INTEGER REFERENCES Facturas,
-nroItem SMALLINT,
-codProducto INTEGER REFERENCES Productos,
-precioUnitario NUMERIC(12,2),
-cantidad INTEGER NOT NULL CHECK (cantidad >0),
-PRIMARY KEY (nroFactura,nroItem)
+(fdet_nroFactura INTEGER REFERENCES Facturas,
+fdet_nroItem SMALLINT,
+fdet_codProducto INTEGER REFERENCES Productos,
+fdet_precioUnitario NUMERIC(12,2),
+fdet_cantidad INTEGER NOT NULL CHECK (cantidad >0),
+PRIMARY KEY (fdet_nroFactura,fdet_nroItem)
 );
 
 /* ********************************************************************************************************* */
