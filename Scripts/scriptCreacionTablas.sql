e491d8e4df252c62c3229b4b6b5f3523c316b40f
Erika Roque <erikaroque2@gmail.com>
2017-03-16 21:39:24 -0300

    correciones

--- Scripts/scriptCreacionTablas.sql
+++ Scripts/scriptCreacionTablas.sql
@@ -3,18 +3,18 @@ USE BDPTF; /*Indico qué base de datos usar*/
 /*Creación de tablas*/
 
 CREATE TABLE Barrios
-(ba_codBarrio SMALLINT PRIMARY KEY AUTO_INCREMENT,
+(ba_idBarrio SMALLINT PRIMARY KEY AUTO_INCREMENT UNIQUE,
 ba_nombreBarrio VARCHAR(60)
 );
 
 CREATE TABLE Productos
-(pro_codProduto SMALLINT PRIMARY KEY,
+(pro_idProduto SMALLINT PRIMARY KEY UNIQUE,
 pro_nombreProducto VARCHAR(60) NOT NULL,
 pro_precioUnitario NUMERIC(12,2)
 );
 USE BDPTF;
 CREATE TABLE Clientes
-(cli_codCliente INTEGER PRIMARY KEY AUTO_INCREMENT,
+(cli_idCliente INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 cli_nombre VARCHAR(60) NOT NULL,
 cli_apellido VARCHAR(60) NOT NULL,
 cli_nroCuit BIGINT UNIQUE,
@@ -28,19 +28,19 @@ cli_telefono VARCHAR(60)
 /* codCliente, nombre, apellido, nroCuit, empresa, direccion1, codPronvincia, ciudad, codPostal, telefono */
 
 CREATE TABLE Facturas
-(f_nroFactura INTEGER PRIMARY KEY AUTO_INCREMENT,
+(f_idFactura INTEGER PRIMARY KEY AUTO_INCREMENT UNIQUE,
 f_fechaFactura DATE NOT NULL,
 f_codCliente INTEGER NOT NULL REFERENCES Clientes,
 f_fechaVencimiento DATE NOT NULL CHECK (fechaVencimiento >= fechaFactura)
 );
 USE BDPTF;
 CREATE TABLE FacturasDetalle
-(fdet_nroFactura INTEGER REFERENCES Facturas,
-fdet_nroItem SMALLINT,
+(fdet_idFactura INTEGER REFERENCES Facturas,
+fdet_nroItem SMALLINT UNIQUE,
 fdet_codProducto INTEGER REFERENCES Productos,
-fdet_precioUnitario NUMERIC(12,2),
 fdet_cantidad INTEGER NOT NULL CHECK (cantidad >0),
-PRIMARY KEY (fdet_nroFactura,fdet_nroItem)
+fdet_importe NUMERIC(12,2),
+PRIMARY KEY (fdet_idFactura,fdet_nroItem)
 );
 
 /* ********************************************************************************************************* */
